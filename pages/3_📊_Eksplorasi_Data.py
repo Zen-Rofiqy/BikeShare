0d2de9e1071f3319e8c9049d10af1eb7633aa45f
Zen-Rofiqy <zenr.prog@gmail.com>
2024-03-08 04:38:57 +0700

    Update

--- pages/3_📊_Eksplorasi_Data.py
+++ pages/3_📊_Eksplorasi_Data.py
@@ -319,6 +319,8 @@ with col1:
     # Menghitung jumlah hari kerja/weekday
     weekday_count = dw_df[(dw_df['holiday'] == '-') & (dw_df['workingday'] == 'WeekDay')].shape[0] + dw_df[(dw_df['holiday'] == 'Libur') & (dw_df['workingday'] == 'WeekDay')].shape[0]
 
+    st.dataframe(holiday_weekend_count)
+    st.dataframe(weekday_count)
     # Membuat Pie Chart
     fig, ax = plt.subplots(figsize=(8, 8))
 
